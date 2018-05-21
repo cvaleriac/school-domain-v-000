4b98a02e64be1334a82756e4f0aebe23ecbda2ab
valeria campilongo <cvaleriac@hotmail.it>
2018-05-21 16:30:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,25 @@
 Class School
   attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
     @roster = {}
-  end 
-  
+  end
+
   def add_student(student, level)
     roster[level] ||= []
     roster[level] << student
-  end 
-  
-def sort 
+  end
+
+def sort
   nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
   nu_hash
-end 
-  
-end 
+end
+
+end
 
 
 
